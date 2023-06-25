9b7a91dbcd330d7a508540d4f3b2d2cede127ddc
earthtones22 <127125432+earthtones22@users.noreply.github.com>
2023-06-25 10:15:04 -0400

    Add files via upload

---
+++ loading.js
@@ -0,0 +1,35 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const scene = document.querySelector('a-scene');
+    const splash = document.querySelector('#splash');
+    const loading = document.querySelector('#splash .loading');
+    const startButton = document.querySelector('#splash .start-button');
+
+    const emitEvent = (eventName, listeners) => {
+        listeners.forEach((listener) => {
+            const el = document.querySelector(listener);
+            el.emit(eventName);
+        })
+    };
+
+    const activateSoundsForTouch = () => {
+    	const sounds = document.querySelectorAll('a-sound')
+        sounds.forEach((soundEl) => {
+            soundEl.components.sound.playSound();
+            soundEl.components.sound.stopSound();
+        })
+    };
+
+    scene.addEventListener('loaded', function (e) {
+        setTimeout(() => {
+            loading.style.display = 'none';
+            splash.style.backgroundColor = 'rgba(0, 0, 0, 0.85)';
+            startButton.style.opacity = 1;
+        }, 50);
+    });
+
+    startButton.addEventListener('click', function (e) {
+        activateSoundsForTouch();
+        splash.style.display = 'none';
+        emitEvent('scene-started', ['#scene']);
+    });
+});
